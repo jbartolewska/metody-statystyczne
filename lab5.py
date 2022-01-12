a53c2d74d1f7e269928a7e81d09ff9c4d3662b27
Julitta <juliett.bartolewska@gmail.com>
2022-01-12 13:31:21 +0100

    Lab #5

--- lab5.py
+++ lab5.py
@@ -7,22 +7,28 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import classification_report, confusion_matrix
 
-attr = ['Sequence Name', 'mcg', 'gvh', 'alm', 'mit', 'erl', 'pox', 'vac', 'nuc']
+attr = ['mcg', 'gvh', 'alm', 'mit', 'erl', 'pox', 'vac', 'nuc', 'class']
 dataset = pd.read_csv('yeast.data', names=attr, delim_whitespace=True)
-dataset['nuc'] = LabelEncoder().fit_transform(dataset['nuc'])
+dataset['class'] = LabelEncoder().fit_transform(dataset['class'])
 
 X = dataset.iloc[:, 0:7].values
 y = dataset.iloc[:, 8].values
 
-# (1)
+# (1) i (2)
 # losowy podział zbioru
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
 #                                                     random_state=0)
 
 # klasyfikacja z walidacją krzyżową
+print('CLassification using kNN')
 # wersja nr 1
-print('WERSJA #1')
+print('VERSION #1')
 metrics = ['minkowski', 'euclidean', 'cosine']
 for metric in metrics:
     print('Metric - {}'.format(metric))
@@ -44,7 +50,7 @@ for metric in metrics:
         print('     avg accuracy : {}'.format(sum(acc_scores) / k))
 
 # wersja nr 2
-print('WERSJA #2')
+print('VERSION #2')
 metrics = ['minkowski', 'euclidean', 'cosine']
 for metric in metrics:
     print('Metric - {}'.format(metric))
@@ -60,8 +66,8 @@ for metric in metrics:
 # jak zwiększyć accuracy?
 # więcej sąsiadów lub podział zbioru na większą liczbę podzbiorów
 
-# (2) klasyfikacja przy pomocy drzewa
-print('CLassification with decision tree')
+# (3) klasyfikacja przy pomocy drzewa
+print('\nCLassification with decision tree')
 for k in range(3, 7):
     print(' k = {}'.format(k))
     kf = KFold(n_splits=k, random_state=None)
@@ -82,6 +88,52 @@ for k in range(3, 7):
 
     print('     accuracy of each fold - {}'.format(acc_scores))
     print('     avg accuracy : {}'.format(sum(acc_scores) / k))
-    tree.plot_tree(classifier)
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(15, 8), dpi=300)
+    tree.plot_tree(classifier, max_depth=3, feature_names=dataset.columns[:-1].values, filled=True, fontsize=5)
+    plt.savefig('lab5_tree_k{}.png'.format(k))
+    # plt.show()
 
-    
+# (4) inne klasyfikatory sklearn z domyślnymi ustawieniami
+# RandomForestClassifier
+print('\nCLassification with random forest')
+for k in range(3, 7):
+    print(' k = {}'.format(k))
+    kf = KFold(n_splits=k, random_state=None)
+    classifier = RandomForestClassifier()
+    acc = cross_val_score(classifier, X, y, cv=kf)
+    print('     accuracy of each fold - {}'.format(acc))
+    print('     avg accuracy : {}'.format(acc.mean()))
+
+# Support Vector Machines (SVM)
+print('\nCLassification with SVM')
+for k in range(3, 7):
+    print(' k = {}'.format(k))
+    kf = KFold(n_splits=k, random_state=None)
+    classifier = SVC()
+    acc = cross_val_score(classifier, X, y, cv=kf)
+    print('     accuracy of each fold - {}'.format(acc))
+    print('     avg accuracy : {}'.format(acc.mean()))
+
+# Naive Bayes
+print('\nCLassification with Gaussian/Multimonial Naive Bayes')
+for k in range(3, 7):
+    print(' k = {}'.format(k))
+    kf = KFold(n_splits=k, random_state=None)
+    classifier = MultinomialNB()
+    acc = cross_val_score(classifier, X, y, cv=kf)
+    print('     accuracy of each fold - {}'.format(acc))
+    print('     avg accuracy : {}'.format(acc.mean()))
+
+# Linear Discriminant Analysis (LDA)
+print('\nCLassification with LDA')
+for k in range(3, 7):
+    print(' k = {}'.format(k))
+    kf = KFold(n_splits=k, random_state=None)
+    classifier = LinearDiscriminantAnalysis()
+    acc = cross_val_score(classifier, X, y, cv=kf)
+    print('     accuracy of each fold - {}'.format(acc))
+    print('     avg accuracy : {}'.format(acc.mean()))
+
+# evaluation
+# print(confusion_matrix(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
